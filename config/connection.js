bf62b7d2ea74aaae016741d104952ca91edb5383
Brian Zauner <binrz00@hotmail.com>
2020-01-07 19:04:01 -0600

    everything is broken

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("burgers_db", "root", "root", {
+  host: "localhost",
+  port: 3306,
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+  define: {
+    timestamps: false
+},
+logging:false
+});
+module.exports = sequelize;
