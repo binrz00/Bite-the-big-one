52e68318c3e40489c0447e51a539d4b4b0be654f
Brian Zauner <binrz00@hotmail.com>
2020-01-08 09:42:45 -0600

    jackson fixed everything

--- server.js
+++ server.js
@@ -2,16 +2,20 @@ const db = require("./models");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require("path")
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Static directory
-app.use(express.static("app/public"));
+app.use(express.static("public"));
 
 // Routes
 require("./routes/api-routes")(app);
+app.get("/", function(req, res){
+  res.sendFile("index.html")
+})
 // Starts the server to begin listening
 db.sequelize.sync({ force: true }).then(function () {
   app.listen(PORT, function () {
